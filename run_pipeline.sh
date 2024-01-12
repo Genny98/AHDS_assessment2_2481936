c0dfe2730e0374813f4d500ee8074ac6b296309b
Genny98 <ue23765@bristol.ac.uk>
2024-01-12 03:22:17 +0000

    Add pipeline script for automated execution

---
+++ run_pipeline.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Navigate to your project directory
+cd /Users/geneviveagyapong/Documents/AHDS_assessment2_2481936
+
+# Run R scripts for data processing and analysis
+Rscript code/merged_data.R
+Rscript visualisation/shiny.R
